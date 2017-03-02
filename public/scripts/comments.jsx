ff547b56bb3e1c331c9c33bc3dc8a0c8c5bd6977
AaronFlower <haojunzhan@gmail.com>
2017-03-02 22:39:12 +0800

    Add comment list for data

---
+++ public/scripts/comments.jsx
@@ -0,0 +1,43 @@
+var data = [
+  {id: 1, author: 'Easonzhan', text: 'We are not enough to getting older'},
+  {id: 2, author: 'Andrew Ng', text: 'Machine learning '}
+]
+var Comment = React.createClass({
+  rawMarkup: function() {
+    var md = new Remarkable();
+    var rawMarkup = md.render(this.props.children.toString());
+    return { __html: rawMarkup };
+  },
+
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
+});
+var CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentList data={data} />,
+  document.getElementById('content')
+);
