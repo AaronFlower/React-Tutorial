e422194a2a4f6fa461ac7e4e509942c2355336a3
easonzhan <easonzhan@tencent.com>
2019-06-06 21:33:34 +0800

    add redux

--- other-apps/02-react-with-ts/src/index.tsx
+++ other-apps/02-react-with-ts/src/index.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Hello from './components/StatefulHello'
+import { createStore } from 'redux';
+import { enthusiasm } from './reducers/index';
+import { StoreState } from './types/index'
+import { Provider } from 'react-redux';
+import { EnthusiasmAction } from './actions';
+/* import Hello from './components/Hello' */
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore<StoreState, EnthusiasmAction, any, any>(
+    enthusiasm, {
+        enthusiasmLevel: 1,
+        languageName: 'TypeScript',
+    }
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Hello name="TypeScript" enthusiasmLevel={8} />
+    </Provider>,
+    document.getElementById('root') as HTMLElement
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
